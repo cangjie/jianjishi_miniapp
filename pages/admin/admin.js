371acbdae2e35a3c0e04893ea54e69e94830a5ec
cangjie <cangjie@hotmail.com>
2023-12-06 17:28:21 +0800

    add admin page

---
+++ pages/admin/admin.js
@@ -0,0 +1,71 @@
+// pages/admin/admin.js
+const app = getApp()
+Page({
+
+  /**
+   * Page initial data
+   */
+  data: {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page load
+   */
+  onLoad(options) {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page is initially rendered
+   */
+  onReady() {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page show
+   */
+  onShow() {
+    var that = this
+    app.loginPromise.then(function(resolve){
+      console.log('user info', app.globalData)
+      that.setData({userInfo: app.globalData.userInfo})
+    })
+  },
+
+  /**
+   * Lifecycle function--Called when page hide
+   */
+  onHide() {
+
+  },
+
+  /**
+   * Lifecycle function--Called when page unload
+   */
+  onUnload() {
+
+  },
+
+  /**
+   * Page event handler function--Called when user drop down
+   */
+  onPullDownRefresh() {
+
+  },
+
+  /**
+   * Called when page reach bottom
+   */
+  onReachBottom() {
+
+  },
+
+  /**
+   * Called when user click on the top right corner to share
+   */
+  onShareAppMessage() {
+
+  }
+})
